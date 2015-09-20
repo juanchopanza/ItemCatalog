bf677085c84221b8a176e52583694db39320ce29
Juan Palacios <juan.palacios@epfl.ch>
2015-09-20 12:27:45 +0200

    Basic Google Oauth2 sign-in.
    Taken from Udacity Authentication and Authorization Restaurant Menus
    app.

--- itemcatalog/__init__.py
+++ itemcatalog/__init__.py
@@ -1,5 +1,6 @@
 '''Main Flask app'''
 from flask import Flask
+from flask import session as login_session
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
